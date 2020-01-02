e8deeff3023ecc06ceace842dda94128b95cb26d
Hasan Abdullah <hasan.cse91@gmail.com>
2020-01-02 09:03:57 +0600

    initial commit

---
+++ app/src/main/java/com/hellohasan/weatherappmvvm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hellohasan.weatherappmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
