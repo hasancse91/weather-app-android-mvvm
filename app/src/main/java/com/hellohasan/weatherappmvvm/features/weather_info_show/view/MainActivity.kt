cded4f778a6abdb85a51bad3028ebd9d90effb1a
Hasan Abdullah <hasan.cse91@gmail.com>
2020-01-10 15:45:21 +0600

    activity oncreate method refactor

--- app/src/main/java/com/hellohasan/weatherappmvvm/features/weather_info_show/view/MainActivity.kt
+++ app/src/main/java/com/hellohasan/weatherappmvvm/features/weather_info_show/view/MainActivity.kt
@@ -12,6 +12,7 @@ import com.hellohasan.weatherappmvvm.R
 import com.hellohasan.weatherappmvvm.features.weather_info_show.model.WeatherInfoShowModel
 import com.hellohasan.weatherappmvvm.features.weather_info_show.model.WeatherInfoShowModelImpl
 import com.hellohasan.weatherappmvvm.features.weather_info_show.model.data_class.City
+import com.hellohasan.weatherappmvvm.features.weather_info_show.model.data_class.WeatherData
 import com.hellohasan.weatherappmvvm.features.weather_info_show.viewmodel.WeatherInfoViewModel
 import com.hellohasan.weatherappmvvm.utils.convertToListOfCityName
 import kotlinx.android.synthetic.main.activity_main.*
@@ -37,17 +38,24 @@ class MainActivity : AppCompatActivity() {
         // fetch city list when Activity open
         viewModel.getCityList(model)
 
+        setLiveDataListeners()
+
+        setViewClickListener()
+    }
+
+    private fun setViewClickListener() {
+        // View Weather button click listener
+        btn_view_weather.setOnClickListener {
+            val selectedCityId = cityList[spinner.selectedItemPosition].id
+            viewModel.getWeatherInfo(selectedCityId, model) // fetch weather info
+        }
+    }
+
+    private fun setLiveDataListeners() {
+
         // observe city list success data
         viewModel.cityListLiveData.observe(this, Observer { cityList ->
-            this.cityList = cityList
-
-            val arrayAdapter = ArrayAdapter(
-                this,
-                R.layout.support_simple_spinner_dropdown_item,
-                cityList.convertToListOfCityName()
-            )
-
-            spinner.adapter = arrayAdapter
+            setCityListSpinner(cityList)
         })
 
         // observe city list fetching failure
@@ -65,21 +73,7 @@ class MainActivity : AppCompatActivity() {
 
         // observe weather info fetching success
         viewModel.weatherInfoLiveData.observe(this, Observer { weatherData ->
-            output_group.visibility = View.VISIBLE
-            tv_error_message.visibility = View.GONE
-
-            tv_date_time?.text = weatherData.dateTime
-            tv_temperature?.text = weatherData.temperature
-            tv_city_country?.text = weatherData.cityAndCountry
-            Glide.with(this).load(weatherData.weatherConditionIconUrl).into(iv_weather_condition)
-            tv_weather_condition?.text = weatherData.weatherConditionIconDescription
-
-            tv_humidity_value?.text = weatherData.humidity
-            tv_pressure_value?.text = weatherData.pressure
-            tv_visibility_value?.text = weatherData.visibility
-
-            tv_sunrise_time?.text = weatherData.sunrise
-            tv_sunset_time?.text = weatherData.sunset
+            setWeatherInfo(weatherData)
         })
 
         // observe weather info fetching failure
@@ -88,11 +82,35 @@ class MainActivity : AppCompatActivity() {
             tv_error_message.visibility = View.VISIBLE
             tv_error_message.text = errorMessage
         })
+    }
 
-        // View Weather button click listener
-        btn_view_weather.setOnClickListener {
-            val selectedCityId = cityList[spinner.selectedItemPosition].id
-            viewModel.getWeatherInfo(selectedCityId, model) // fetch weather info
-        }
+    private fun setCityListSpinner(cityList: MutableList<City>) {
+        this.cityList = cityList
+
+        val arrayAdapter = ArrayAdapter(
+            this,
+            R.layout.support_simple_spinner_dropdown_item,
+            this.cityList.convertToListOfCityName()
+        )
+
+        spinner.adapter = arrayAdapter
+    }
+
+    private fun setWeatherInfo(weatherData: WeatherData) {
+        output_group.visibility = View.VISIBLE
+        tv_error_message.visibility = View.GONE
+
+        tv_date_time?.text = weatherData.dateTime
+        tv_temperature?.text = weatherData.temperature
+        tv_city_country?.text = weatherData.cityAndCountry
+        Glide.with(this).load(weatherData.weatherConditionIconUrl).into(iv_weather_condition)
+        tv_weather_condition?.text = weatherData.weatherConditionIconDescription
+
+        tv_humidity_value?.text = weatherData.humidity
+        tv_pressure_value?.text = weatherData.pressure
+        tv_visibility_value?.text = weatherData.visibility
+
+        tv_sunrise_time?.text = weatherData.sunrise
+        tv_sunset_time?.text = weatherData.sunset
     }
 }
